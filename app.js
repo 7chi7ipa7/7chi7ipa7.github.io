da392d2ed2b9680a88026e526ef54cc31b67af2d
Artem <62651528+7chi7ipa7@users.noreply.github.com>
2023-01-16 19:34:19 +0300

    Start commit
    add 4 files

---
+++ app.js
@@ -0,0 +1,57 @@
+(function ($) {
+    $.fn.redraw = function () {
+        return this.map(function () { this.offsetTop; return this; });
+    };
+})(jQuery);
+
+let tg = window.Telegram.WebApp;
+tg.expand();
+
+
+var app = {
+    appValue: 0,
+
+    init: function () {
+        tg.ready();
+        $('body').show();
+
+        if (!tg.initDataUnsafe ||
+            !tg.initDataUnsafe.query_id) {
+            app.isClosed = true;
+            $('body').addClass('closed');
+            app.showStatus('You opened pages not through telegram');
+            return;
+        }
+        app.userId = tg.initDataUnsafe.user.id
+        app.userName = tg.initDataUnsafe?.user?.username
+        initApp();
+    },
+    showStatus: function (text) {
+        clearTimeout(app.statusTo);
+        $('.js-status').text(text).addClass('shown');
+    }
+}
+
+function initApp() {
+    let hi_btn = document.getElementById('btn1');
+
+    hi_btn.addEventListener('click', function () {
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            app.appValue++
+            tg.MainButton.setText(`Hi ${app.userName}`)
+            tg.MainButton.show();
+        }
+    })
+
+    tg.onEvent('mainButtonClicked', function () {
+        let data = {
+            id: app.userId,
+            name: app.userName,
+            value: app.appValue,
+        };
+
+        tg.sendData(data);
+    });
+}
